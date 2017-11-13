a86d13f5114c49bb6f1ca49fa25ce6e54f57bff8
yangc20013 <yangc20013@github.com>
2017-11-13 09:29:35 +0800

    userRepository,新增自定义的HQL

--- hibernate/src/main/java/com/springboot/repository/UserRepository.java
+++ hibernate/src/main/java/com/springboot/repository/UserRepository.java
@@ -1,12 +1,18 @@
 package com.springboot.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.springboot.entity.User;
 
-import org.springframework.data.repository.CrudRepository;
-
 @Repository
 public interface  UserRepository extends CrudRepository<User, Long> {
-
+	@Query("select a from User a left join a.roles r where r.name = 'ADMIN'")
+	List<User> findUser();
+	
+//	@Query("select r from CachedMtdPerformanceDataDtvds r where r.hrId = ?1 and r.firstDate = ?2 order by r.ach desc")
+//	List<? extends CachedPerformanceDataDtvds> findByHrIdAndFirstDate(String hrId, Calendar firstDayOfMonth);
 }
